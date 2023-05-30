9cdea2f88f4c477383843e53904d8826fdab1479
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-05-30 23:31:21 +0300

    class App

---
+++ src/components/services/services.js
@@ -0,0 +1,5 @@
+//const API_KEY = '35113425-894140f70267936d7d418e310';
+//const BASE_URL = 'https://pixabay.com/api/';
+
+//export const getCards = ()
+//`${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${searchValue}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
