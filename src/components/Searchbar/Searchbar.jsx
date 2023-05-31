3807fd81c1c035fc46e7a4ada35f6fea618bc0b2
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-05-31 16:59:08 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,1 +1,24 @@
-
+import React from 'react';
+//import {Component} from React;
+export class getImages extends Comment {
+  render() {
+    return (
+      <>
+        <header class="searchbar">
+          <form class="form">
+            <button type="submit" class="button">
+              <span class="button-label">Search</span>
+            </button>
+            <input
+              class="input"
+              type="text"
+              autocomplete="off"
+              autofocus
+              placeholder="Search images and photos"
+            />
+          </form>
+        </header>
+      </>
+    );
+  }
+}
