503a4884f489eb0f12c9ab97382813a97d3f8e66
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-06-05 13:20:02 +0300

    delete ImageGallery

--- src/services/services.js
+++ src/services/services.js
@@ -1,15 +1,15 @@
 const API_KEY = '35113425-894140f70267936d7d418e310';
-const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api/?';
 
-const fetchAPI = (value, page) => {
+const fetchAPI = (searchText, page = 1) => {
   fetch(
-    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${value}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
+    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${searchText}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
   ).then(response => {
     if (response.ok) {
       return response.json();
     }
     return Promise.reject(
-      new Error(`There are no ${value} images matching your search`)
+      new Error(`There are no ${searchText} images matching your search`)
     );
   });
 };
